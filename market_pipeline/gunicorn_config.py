71a3a336723754c10e5d32ace84d16ff1a066d12
shima-galerie <miladnd01@gmail.com>
2026-03-21 22:36:27 +0100

    Add Gunicorn configuration file

---
+++ market_pipeline/gunicorn_config.py
@@ -0,0 +1,19 @@
+# gunicorn_config.py
+import multiprocessing
+
+# Bind
+bind = "0.0.0.0:8080"
+
+# Workers
+workers = 2
+worker_class = "gthread"
+threads = 4
+
+# Timeout
+timeout = 120
+keepalive = 5
+
+# Logging
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
